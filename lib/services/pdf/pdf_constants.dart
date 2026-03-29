45b47e8af96249c7948f037ab1ad39cdcdba0da6
Nikhar Tale <nikhartale12@gmail.com>
2026-03-30 00:25:16 +0530

    improve logo & footer text visibility

--- lib/services/pdf/pdf_constants.dart
+++ lib/services/pdf/pdf_constants.dart
@@ -3,4 +3,5 @@ class PdfConstants {
   static const String branchPhone = "919425411288";
   static const String branchEmail = "awinashnigam@gmail.com";
   static const String websiteUrl = "curemixhealthcare.in";
+  static const String googlePlayUrl = "https://play.google.com/store/apps/details?id=com.nikhartale.curemix_healthcare"; // Assuming this ID based on previous context
 }
