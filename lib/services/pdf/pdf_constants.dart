6e64d5b1b780e8efd94e8e54803ba8d3eca0c191
Nikhar Tale <nikhartale12@gmail.com>
2026-03-29 19:50:26 +0530

    change the number

--- lib/services/pdf/pdf_constants.dart
+++ lib/services/pdf/pdf_constants.dart
@@ -1,6 +1,6 @@
 class PdfConstants {
   static const String branchName = "Curemix Healthcare";
-  static const String branchPhone = "9425411288";
+  static const String branchPhone = "919425411288";
   static const String branchEmail = "awinashnigam@gmail.com";
   static const String websiteUrl = "curemixhealthcare.in";
 }
